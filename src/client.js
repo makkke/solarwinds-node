37e5089576e6e7834a0f732e2f1b8e8d93c52542
Viatcheslav Ivanov <slava.eth@gmail.com>
2016-06-24 20:17:23 -0700

    refactor(cli): update cli commands to a new format (#8)
    Closes #6

--- src/client.js
+++ src/client.js
@@ -2,9 +2,9 @@ import request from 'request'
 import { camelize } from './utils'
 
 class Client {
-  constructor(username, password) {
+  constructor(username, password, url) {
     this.request = request.defaults({
-      baseUrl: process.env.SOLARWINDS_URL || 'https://swsam.surrey.ca:17778/SolarWinds/InformationService/V3/Json',
+      baseUrl: `${url}/SolarWinds/InformationService/V3/Json`,
       auth: { username, password },
       json: true,
       strictSSL: false,
--- src/solarwinds.js
+++ src/solarwinds.js
@@ -1,16 +1,19 @@
 import Client from './client'
 import Nodes from './nodes'
 
+/* eslint-disable */
 if (!global._babelPolyfill) {
   require('babel-polyfill')
 }
+/* eslint-disable */
 
 const SOLARWINDS_USERNAME = process.env.SOLARWINDS_USERNAME
 const SOLARWINDS_PASSWORD = process.env.SOLARWINDS_PASSWORD
+const SOLARWINDS_URL = process.env.SOLARWINDS_URL
 
 class Landscape {
-  constructor(username = SOLARWINDS_USERNAME, password = SOLARWINDS_PASSWORD) {
-    this.client = new Client(username, password)
+  constructor(username = SOLARWINDS_USERNAME, password = SOLARWINDS_PASSWORD, url = SOLARWINDS_URL) {
+    this.client = new Client(username, password, url)
     this.nodes = new Nodes(this.client)
   }
 }
