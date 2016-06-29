2fe0b156038a8f7e9b8520ed41fa945d11cfc467
Viatcheslav Ivanov <viatcheslav.ivanov@surrey.ca>
2016-06-29 10:27:14 -0700

    chore: refactor based on code review

--- src/solarwinds.js
+++ src/solarwinds.js
@@ -20,10 +20,10 @@ class Landscape {
     username = SOLARWINDS_USERNAME, password = SOLARWINDS_PASSWORD,
     hostname = SOLARWINDS_HOSTNAME, port = SOLARWINDS_PORT
   ) {
-    if (is.undefined(username)) throw new Error('Provide username for SolarWinds or env variable SOLARWINDS_USERNAME')
-    if (is.undefined(password)) throw new Error('Provide password for SolarWinds or env variable SOLARWINDS_PASSWORD')
-    if (is.undefined(hostname)) throw new Error('Provide hostname of SolarWinds API or env variable SOLARWINDS_HOSTNAME')
-    if (is.undefined(port)) throw new Error('Provide port of SolarWinds API or env variable SOLARWINDS_PORT')
+    if (is.undefined(username) || is.empty(username)) throw new Error('Provide username for SolarWinds or env variable SOLARWINDS_USERNAME')
+    if (is.undefined(password) || is.empty(password)) throw new Error('Provide password for SolarWinds or env variable SOLARWINDS_PASSWORD')
+    if (is.undefined(hostname) || is.empty(hostname)) throw new Error('Provide hostname of SolarWinds API or env variable SOLARWINDS_HOSTNAME')
+    if (is.undefined(port) || is.empty(port)) throw new Error('Provide port of SolarWinds API or env variable SOLARWINDS_PORT')
 
     this.client = new Client(username, password, hostname, port)
 
